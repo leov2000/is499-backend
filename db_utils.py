603ba5735327f2bfb41556646141935d006b338e
Leo Vergara <leovergarah@gmail.com>
2020-12-08 15:51:23 -0500

    (Added) utils for db

---
+++ db_utils.py
@@ -0,0 +1,36 @@
+import sqlite3
+import csv
+
+def create_tables():
+    conn = sqlite3.connect("schools.db")
+    sql_file = open("schema.sql")
+    sql_tables = sql_file.read()
+
+    conn.executescript(sql_tables)
+    conn.commit()
+    conn.close()
+
+    sql_file.close()
+
+def connect_and_insert(insert_str):
+    conn = sqlite3.connect("schools.db")
+    cursor = conn.cursor()
+
+    cursor.executescript(insert_str)
+    conn.commit()
+    conn.close()
+
+
+def csv_to_list(csv_name):
+    with open(csv_name) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        csv_list = [row for row in reader]
+
+        return csv_list
+
+
+def split_data(csv_list):
+    header = csv_list[:1][0]
+    data = csv_list[1:]
+
+    return (header, data)
