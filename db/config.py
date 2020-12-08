0ab806ae4e185d8907d9e61bfce20074304a6d3e
Leo Vergara <leovergarah@gmail.com>
2020-12-08 16:30:09 -0500

    (Added) headers tuple config

---
+++ db/config.py
@@ -0,0 +1,61 @@
+headers = {
+    "all": (
+        "dbn",
+        "school_name",
+        "grade",
+        "year",
+        "demographic_category",
+        "demographic_variable",
+        "num_total_days",
+        "num_days_absent",
+        "num_days_present",
+        "percent_attendance",
+        "num_chronically_absent",
+        "percent_chronically_absent",
+        "complaints",
+        "material_incidents",
+        "location",
+        "postal_code",
+        "coordinates"
+        ),
+    "abscenses": (
+        "dbn",
+        "school_name",
+        "grade",
+        "year",
+        "demographic_category",
+        "demographic_variable",
+        "num_total_days",
+        "num_days_absent",
+        "num_days_present",
+        "percent_attendance",
+        "num_chronically_absent",
+        "percent_chronically_absent"
+        ),
+    "consoldidated": (
+        "dbn",
+        "school_name",
+        "grade",
+        "year",
+        "demographic_category",
+        "demographic_variable",
+        "num_total_days",
+        "num_days_absent",
+        "num_days_present",
+        "percent_attendance",
+        "num_chronically_absent",
+        "percent_chronically_absent"
+        ),
+    "bullying": (
+        "dbn",
+        "school_name",
+        "complaints",
+        "material_incidents"
+    ),
+    "schools": (
+        "dbn",
+        "location",
+        "postal_code",
+        "coordinates"
+    )
+}
